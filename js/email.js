fcd545e6c116053216a0e9729805da7b115b6a33
ballestero <ballesterojj@gmail.com>
2018-12-10 23:37:04 -0600

    Update email.js

--- js/email.js
+++ js/email.js
@@ -1,18 +1,28 @@
 var nodemailer = require('nodemailer');
 
+/*var name = document.getElementById('name').value,
+    email = document.getElementById('email').value,
+    phone = document.getElementById('phone').value,
+    message = document.getElementById('message').value;
+*/
+var name = 'Jeison',
+    email = 'ballesterojj',
+    phone = '8752',
+    message = 'hello world x';
+
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'ballesterojj@gmail.com',
-        pass: 'sjpsxrtjj'
+        pass: 'xx'
     }
 });
 
 var mailOptions = {
-    from: 'jballesteros@ucenfotec.ac.cr',
+    from: 'jeisonjb1292@hotmail.com',
     to: 'jeisonjb1292@hotmail.com',
-    subject: 'Mensaje Web',
-    text: 'hello world'
+    subject: 'Email New Creative Web',
+    text: `Nombre: ${name} \nCorreo: ${email} \nTeléfono: ${phone} \nMensaje: ${message}`
 };
 
 transporter.sendMail(mailOptions, function(error, info) {
